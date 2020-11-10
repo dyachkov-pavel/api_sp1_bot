db0c4966e91fe4d6ab0db467586b2026b259ba9e
dyachkov-pavel <pahanthebest12345@mail.ru>
2020-11-10 19:41:57 +0300

    exceptions

--- homework.py
+++ homework.py
@@ -17,22 +17,43 @@ PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
+
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    try:
+        homework_name = homework['homework_name']
+        homework_status = homework['status']
+        if homework_status == 'rejected':
+            verdict = 'К сожалению в работе нашлись ошибки.'
+        else:
+            verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    except KeyError as errk:
+        logger.error(f'Key Error: {errk}')
+        raise errk
+    except IndexError as erri:
+        logger.error(f'Index Error: {erri}')
+        raise erri
 
 
 def get_homework_statuses(current_timestamp):
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     params = {'from_date': current_timestamp}
     YANDEX_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
-    homework_statuses = requests.get(
-        YANDEX_URL, headers=headers, params=params)
-    return homework_statuses.json()
+    try:
+        homework_statuses = requests.get(
+            YANDEX_URL, headers=headers, params=params)
+        return homework_statuses.json()
+    except requests.exceptions.HTTPError as errh:
+        logger.error(f'Http Error: {errh}')
+    except requests.exceptions.ConnectionError as errc:
+        logger.error(f'Error Connecting: {errc}')
+    except requests.exceptions.Timeout as errt:
+        logger.error(f'Timeout Error: {errt}')
+    except requests.exceptions.RequestException as err:
+        logger.error(f'OOps: Something Else {err}')
+
 
 
 def send_message(message, bot_client):
@@ -40,41 +61,17 @@ def send_message(message, bot_client):
 
 
 def main():
-    # проинициализировать бота здесь
     tg_bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]), tg_bot)
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(1200)  # опрашивать раз в 20 минут
-        except requests.exceptions.HTTPError as errh:
-            logger.error(f'Http Error: {errh}')
-            time.sleep(5)
-            continue
-        except requests.exceptions.ConnectionError as errc:
-            logger.error(f'Error Connecting: {errc}')
-            time.sleep(5)
-            continue
-        except requests.exceptions.Timeout as errt:
-            logger.error(f'Timeout Error: {errt}')
-            time.sleep(5)
-            continue
-        except KeyError as errk:
-            logger.error(f'Key Error: {errk}')
-            time.sleep(5)
-            continue
-        except IndexError as erri:
-            logger.error(f'Index Error: {erri}')
-            time.sleep(5)
-            continue
-        except requests.exceptions.RequestException as err:
-            logger.error(f'OOps: Something Else {err}')
-            time.sleep(5)
-            continue
+                send_message(parse_homework_status(
+                    new_homework.get('homeworks')[0]), tg_bot)
+            current_timestamp = new_homework.get('current_date')
+            time.sleep(1200)
         except Exception as e:
             logger.error(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
